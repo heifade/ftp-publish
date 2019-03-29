770bbc3fc806cbfd2a8619403d5029440f097d2a
heifade <heifade@126.com>
2019-03-29 22:08:16 +0800

    init

---
+++ rollup.config.js
@@ -0,0 +1,13 @@
+import json from 'rollup-plugin-json';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import typescript from 'rollup-plugin-typescript';
+
+export default {
+  input: 'src/index.ts',
+  output: {
+    file: 'bin/index.js',
+    format: 'cjs',
+  },
+  plugins: [typescript(), json(), resolve(), commonjs()],
+};
---
+++ src/ftpUpload.ts
@@ -0,0 +1,43 @@
+import chalk from 'chalk';
+import { stdout as singleLineLog } from 'single-line-log';
+import ftp from 'basic-ftp';
+
+export interface UploadProps {
+  host: string;
+  port: number;
+  user: string;
+  password: string;
+  sourcePath: string;
+  ftpPath: string;
+}
+
+export async function upload(pars: UploadProps) {
+  let client = new ftp.Client(600 * 1000);
+  // client.ftp.verbose = true;
+
+  try {
+    await client.access({
+      host: pars.host,
+      port: pars.port,
+      user: pars.user,
+      password: pars.password,
+      secure: false,
+    });
+
+    client.trackProgress(info => {
+      singleLineLog(`正在上传文件:${info.name}, 已上传:${info.bytesOverall}\n`);
+    });
+
+    await client.ensureDir(pars.ftpPath);
+    await client.clearWorkingDir();
+    await client.uploadDir(pars.sourcePath);
+
+    client.trackProgress();
+
+    singleLineLog(chalk.green('上传完成!') + '\n');
+  } catch (err) {
+    singleLineLog(chalk.red('上传失败！', err) + '\n');
+  }
+
+  await client.close();
+}
